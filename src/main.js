4168e70c1d9fe9af86958b81578685f5f2fa00a9
DESKTOP-F0N1MCU\TatsuyaIshii <godmakemefunky1989@gmail.com>
2017-10-16 01:08:45 +0900

    Add comment

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
 $(function() {
+  // x-handlebars-templateのscriptを取得
   const SOURCE = $('#template').html();
-  const TEMPLATE = #('#template').html();
+  // Handlebarsのcompileメソッドにx-handlebars-templateのscriptを渡す
+  const TEMPLATE = Handlebars.compile(SOURCE);
+  // valueオブジェクトのプロパティに各値を記述
+  let values = {
+    name : 'Satoshi Watanabe',
+    age : '33'
+  };
 
-})
+  console.log(TEMPLATE);
+  // TEMPLATEの返り値がメソッドなので、引数にvaluesオブジェクトを入れてあげる
+  const HTML = TEMPLATE(values);
+  console.log(HTML);
+  $('#contents').html(HTML);
+});
